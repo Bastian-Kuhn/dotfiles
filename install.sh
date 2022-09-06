cdb5a99fd46a69771530f18aed2d17ee221085c1
Bastian Kuhn <mail@bastian-kuhn.de>
2022-09-06 11:23:35 +0200

    Added Tmux to Installer

--- install.sh
+++ install.sh
@@ -5,4 +5,15 @@ cd $GIT_PATH
 while read line; do
     git clone $line
 done < gitsources
+cd ~
+rm .vimrc
+rm -r .vim
+ln -s ~/git/dotfiles/vim/vimrc .vimrc
+ln -s ~/git/dotfiles/vim/vim .vim
+
+
+echo "Setup TMUX"
+cd ~
+rm .tmuxrc
+ln -s ~/git/dotfiles/tmux/tmuxrc .tmuxrc
 
